b7df684928cbcb8d2f4f3315ecc9ddcffe95b186
SK Kamal <skkamal6301@gmail.com>
2024-03-28 13:03:54 +0600

    Nav Section or Header Logo ypdate

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -12,17 +12,34 @@ use AQUILA_THEME\Inc\Traits\Singleton;
 class AQUILA_THEME
 {
     use Singleton;
-
     protected function __construct()
     {
-        wp_die('Hello');
-
         // load class
-        $this->set_hooks();
+
+        Assets::get_instance();
+
+        $this->setup_hooks();
     }
 
-    protected function set_hooks()
+    protected function setup_hooks()
     {
-        // actions and filters
+        /**
+         * Actions.
+         */
+
+        add_action('after_setup_theme', [$this, 'setup_theme']);
+    }
+
+    public function setup_theme()
+    {
+        add_theme_support('title-tag');
+
+        add_theme_support( 'custom-logo', [
+            'height'               => 100,
+            'width'                => 400,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'header-text'          => ['site-title', 'site-description' ],
+        ] );
     }
 }
